f31729faa7f508741afe8449c4518e939dd9fe9d
creatoxx <49265368+creatoxx@users.noreply.github.com>
2023-11-09 14:43:32 +0100

    resource wheel, settlement icons, discovered state, fixes

--- SFM/Data/Scripts/PreventDefault.js
+++ SFM/Data/Scripts/PreventDefault.js
@@ -1,7 +1,7 @@
 document.addEventListener("keydown", function (event) {
 	if (event.key === 'F11') {
 		event.preventDefault();
-	} else if (event.key === 'F1' || event.key === 'F3' || event.key === 'F6' || event.key === 'F7' || event.key === 'F10') {
+	} else if (event.key === 'F1' ||/* event.key === 'F3' ||*/ event.key === 'F6' || event.key === 'F7' || event.key === 'F10') {
 		event.preventDefault();
 	}
 });		
